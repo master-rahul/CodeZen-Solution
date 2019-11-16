01b1a74b88429e553a5c1115e1cd5e5456411f6b
Rahul Verma <53015177+master-rahul@users.noreply.github.com>
2019-11-16 09:39:19 +0530

    Add files via upload

--- Print The Pattern.cpp
+++ Print The Pattern.cpp
@@ -12,7 +12,25 @@ int main(){
 }
 
 void printPatt(int n){
-	
-
+	int odd=1, even = n-1,num=n-1;
+	if(n%2 ==0)
+		even = n;
+	for(int i=1;i<=n;i++){
+		if(i <=n/2){
+			for(int j=(n*odd) -num;j<= n*odd;j++)
+				cout<<j<<" ";
+			odd +=2;
+		}
+		else if(i == (n/2  +1) && n%2 !=0){
+			for(int j=(n*odd) -num;j<= n*odd;j++)
+				cout<<j<<" ";
+		}
+		else{
+			for(int j=(n*even) -num;j<= n*even;j++)
+				cout<<j<<" ";
+			even -=2;
+		}
+		cout<<endl;
 	}
+	
 }
